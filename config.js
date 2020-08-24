e691dd62ce5cb96f1b1f0d98499e6e9378bf2168
halfpasta <christian.george360@gmail.com>
2020-08-23 21:38:16 -0400

    moved config into src

--- config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    // API_ENDPOINT: 'http://localhost:8000',
-    // API_USER: 'http://localhost:8000/user',
-    // API_MOVIES: 'http://localhost:9090/movie'
-    API_ENDPOINT: 'https://movie-forum-capstone-server.herokuapp.com/',
-    API_USER: 'https://movie-forum-capstone-server.herokuapp.com/user',
-    API_MOVIES: 'https://movie-forum-capstone-server.herokuapp.com/movie'
-  }
